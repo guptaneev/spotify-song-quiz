2da679c686c75792638da81f272b41f5e809f475
ngupta <neevgupta.austin@gmail.com>
2025-01-04 12:06:14 -0600

    Added track (song) searching; input field for track to be searched; prints the searched song from Spotify with the artist name

--- main.py
+++ main.py
@@ -41,6 +41,21 @@ def search_for_artist(token, artist_name):
         return None
     return json_result[0]
 
+def search_for_track(token, track_name):
+    url = f"https://api.spotify.com/v1/search?q={track_name}&type=track&limit=1"
+    headers = get_auth_header(token)
+    
+    query_url = url
+    result = get(query_url, headers = headers)
+    json_result = json.loads(result.content)
+    if len(json_result) == 0:
+        print("No song with this genre exists!")
+        return None
+    artist_name = json_result["tracks"]["items"][0]["album"]["artists"][0]["name"]
+    song_name = json_result["tracks"]["items"][0]["name"]
+    searched_song = [song_name, artist_name]
+    return searched_song
+
 def get_songs_by_artist(token, artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
     headers = get_auth_header(token)
@@ -48,10 +63,8 @@ def get_songs_by_artist(token, artist_id):
     json_result = json.loads(result.content)["tracks"]
     return json_result
 
-token = get_token()
-result = search_for_artist(token, "ACDC")
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
+while True:
+    search_song_input = str(input("What song would you like to search for? "))
+    token = get_token()
+    result = search_for_track(token, search_song_input)
+    print(f"You searched for: {result[0]} by {result[1]}")
