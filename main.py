a1ba2dc8a837b891d2d494cd3eca7f6cccdba810
ngupta <neevgupta.austin@gmail.com>
2025-01-04 21:23:19 -0600

    Adding Song class and SongQuiz class for game functionality

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
+
+class Song:
+    
+    def __init__(self, song_name, artist_name):
+        self.song_name = song_name
+        self.artist_name = artist_name
+    
+    def toString(self):
+        return f"{self.song_name} by {self.artist_name}"
+    
+    
+    
+    
+    
 def get_token():
     auth_string = client_id + ":" + client_secret
     auth_bytes = auth_string.encode("utf-8")
@@ -25,7 +39,7 @@ def get_token():
     json_result = json.loads(result.content)
     token = json_result["access_token"]
     return token
-
+ 
 def get_auth_header(token):
     return {"Authorization": "Bearer " + token}
 
@@ -53,8 +67,7 @@ def search_for_track(token, track_name):
         return None
     artist_name = json_result["tracks"]["items"][0]["album"]["artists"][0]["name"]
     song_name = json_result["tracks"]["items"][0]["name"]
-    searched_song = [song_name, artist_name]
-    return searched_song
+    return Song(song_name, artist_name)
 
 def get_songs_by_artist(token, artist_id):
     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
@@ -63,8 +76,34 @@ def get_songs_by_artist(token, artist_id):
     json_result = json.loads(result.content)["tracks"]
     return json_result
 
-while True:
-    search_song_input = str(input("What song would you like to search for? "))
-    token = get_token()
-    result = search_for_track(token, search_song_input)
-    print(f"You searched for: {result[0]} by {result[1]}")
+
+    
+    
+class SongQuiz:
+    
+    def __init__(self):
+        self.number_of_songs = 0
+        self.songs = []
+        
+    def add_song(self, track_name):
+        searched_song = search_for_track(token, track_name)
+        confirmation = input(f"Would you like to add {searched_song.toString()} to your quiz? (y/n) ")
+        if confirmation == "y":
+            if searched_song not in self.songs:
+                self.songs.append(searched_song)
+                print("Song added!")
+                return True
+            print("Song already added!")
+            return False
+        else:
+            print("Song not added!")
+            return False
+        
+    def start_quiz(self):
+        
+        
+
+search_song_input = str(input("What song would you like to search for? "))
+token = get_token()
+result = search_for_track(token, search_song_input)
+print(f"You searched for: {result[0]} by {result[1]}")
